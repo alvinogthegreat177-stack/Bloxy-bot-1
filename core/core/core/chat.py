5fd39753c1097f82b546fa018c511201b96917eb
alvinogthegreat177-stack <alvinogthegreat177@gmail.com>
2026-05-04 21:19:29 +0300

    chat.py

--- core/core/core/chat.py
+++ core/core/core/chat.py
@@ -1,1 +1,14 @@
+from core.ai_engine import ask_ai
+from core.tools import wiki, dictionary
 
+def handle_chat(user, msg):
+
+    t = msg.lower()
+
+    if "define" in t:
+        return dictionary(msg)
+
+    if "who is" in t:
+        return wiki(msg)
+
+    return ask_ai(msg)
