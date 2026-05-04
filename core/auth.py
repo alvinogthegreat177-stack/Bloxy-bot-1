45ed22051631e1c246b28151ace43d6b15a0a68c
alvinogthegreat177-stack <alvinogthegreat177@gmail.com>
2026-05-04 21:18:45 +0300

    auth.py

--- core/auth.py
+++ core/auth.py
@@ -1,1 +1,36 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import session
+from core.db import cur, db
 
+OWNER = "aTg"
+
+def signup(u, p):
+    if u == OWNER:
+        return False
+
+    cur.execute("INSERT INTO users VALUES (?,?,?)",
+                (u, generate_password_hash(p), 0))
+    db.commit()
+    return True
+
+def login(u, p):
+    cur.execute("SELECT password FROM users WHERE username=?", (u,))
+    row = cur.fetchone()
+
+    if row and check_password_hash(row[0], p):
+        session["user"] = u
+        return True
+    return False
+
+def logout():
+    session.pop("user", None)
+
+def get_user():
+    u = session.get("user")
+    if not u:
+        return {"name": "Guest", "verified": False}
+
+    return {
+        "name": u,
+        "verified": u == OWNER
+    }
