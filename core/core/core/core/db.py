ed022df59c0b7c7766414410c4c932fff0d1df3a
alvinogthegreat177-stack <alvinogthegreat177@gmail.com>
2026-05-04 21:35:23 +0300

    db.py

--- core/core/core/core/db.py
+++ core/core/core/core/db.py
@@ -4,6 +4,29 @@ db = sqlite3.connect("data.db", check_same_thread=False)
 cur = db.cursor()
 
 def init_db():
-    cur.execute("CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT, verified INT)")
-    cur.execute("CREATE TABLE IF NOT EXISTS chats (user TEXT, data TEXT)")
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        username TEXT,
+        password TEXT,
+        verified INT
+    )
+    """)
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS chats (
+        chat_id TEXT,
+        user TEXT,
+        title TEXT
+    )
+    """)
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS messages (
+        chat_id TEXT,
+        user TEXT,
+        role TEXT,
+        content TEXT
+    )
+    """)
+
     db.commit()
